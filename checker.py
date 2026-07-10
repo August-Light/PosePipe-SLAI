5cbcc304c112ce2e371fdeb2838f36661e7c2ada
hmfli0bgzykc7 <dentistlinapple@163.com>
2026-07-10 13:55:33 -0500

    updated checker

--- checker.py
+++ checker.py
@@ -4,82 +4,34 @@
     [(1, 2), (2, 3), (3, 1)]
 ]
 
-for e in cases:
-    n = set()
-    g = {}
-    u = {}
+def check(e, n):
+    g = [[] for _ in range(n)]
+    for i, j in e:
+        g[i-1].append(j-1)
+        g[j-1].append(i-1)
 
-    for a, b in e:
-        n.add(a)
-        n.add(b)
+    visited = [0] * n
 
-        if a not in g:
-            g[a] = []
-        if b not in g:
-            g[b] = []
-        g[a].append(b)
+    def dfs(node, parent):
+        visited[node] = 1
 
-        if a not in u:
-            u[a] = []
-        if b not in u:
-            u[b] = []
-        u[a].append(b)
-        u[b].append(a)
-        
-    def check_connectivity():
-        s = list(n)[0]
-        v = set()
-        stack = [s]
+        for i in g[node]:
+            if visited[i] == 0:
+                dfs(i, node)
+                
+            elif i != parent:
+                return True 
+                
+    
+    has_cycle = dfs(0, -1)
 
-        while stack:
-            x = stack.pop()
-            if x in v:
-                continue
-            v.add(x)
-            for y in u[x]:
-                if y not in v:
-                    stack.append(y)
+    is_connected = 0 not in visited
 
-        return len(v) == len(n)
-    def check_cycles():
-        st = {}
-        for x in n:
-            st[x] = 0
-
-        ok2 = True
-
-        for s in n:
-            if st[s] != 0:
-                continue
-
-            stack = [(s, 0)]
-            st[s] = 1
-
-            while stack and ok2:
-                x, i = stack[-1]
-
-                if i == len(g[x]):
-                    st[x] = 2
-                    stack.pop()
-                else:
-                    y = g[x][i]
-                    stack[-1] = (x, i + 1)
-
-                    if st[y] == 1:
-                        ok2 = False
-                    elif st[y] == 0:
-                        st[y] = 1
-                        stack.append((y, 0))
-
-            if not ok2:
-                break
-        
-        return ok2
-
-    ok1 = check_connectivity()
-    ok2 = check_cycles()
-
-    if ok1 and ok2:
+    if is_connected and not has_cycle:
         print("yes")
     else:
         print("no")
+
+check(cases[0], 4)  
+check(cases[1], 6)  
+check(cases[2], 3)
